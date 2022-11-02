cd5cba25d90fa9e39f54900f7253038cfe820049
LeonardoCardosoDev <leonardo.cardoso@sptech.school>
2022-11-01 23:40:31 -0300

    Feito modal da carta aberta

--- script/letter.js
+++ script/letter.js
@@ -2,3 +2,9 @@ function carta() {
   imgCarta.style.display = "none";
   imgCarta2.style.display = "block";
 }
+function cartaOpen() {
+  let modal = document.querySelector(".modal");
+
+  imgCarta2.style.display = "none";
+  modal.style.display = "flex";
+}
