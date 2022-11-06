c3f67b09521b4e86c864464907c5f34b20d26374
LeonardoCardosoDev <leonardo.cardoso@sptech.school>
2022-11-06 19:52:04 -0300

    Adicionado página pós home

---
+++ site/public/js/map.js
@@ -0,0 +1,24 @@
+function next() {
+  cont1.style.display = "none";
+  cont2.style.display = "flex";
+}
+
+function next2(params) {
+  cont2.style.display = "none";
+  cont3.style.display = "flex";
+}
+function next3(params) {
+  cont3.style.display = "none";
+  cont4.style.display = "flex";
+}
+function next4(params) {
+  cont4.style.display = "none";
+  cont5.style.display = "flex";
+}
+function next5(params) {
+  cont5.style.display = "none";
+  cont6.style.display = "flex";
+}
+function next6(params) {
+  cont6.style.display = "none";
+}
