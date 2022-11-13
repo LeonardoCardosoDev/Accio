61a8bb81dc6c57e810cf513a17e822caf02e6c4c
LeonardoCardosoDev <leonardo.cardoso@sptech.school>
2022-11-13 20:36:59 -0300

    Estilização e funções quiz finalizada

--- site/public/js/quizCasas.js
+++ site/public/js/quizCasas.js
@@ -1,87 +1,157 @@
-/*export default [
-  (questao1 = {
-    questao: "O que prefere ser?",
-    alternativas: [
-      { option: "Apreciado(a)", valor: 20 },
-      { option: "Confiável", valor: 20 },
-      { option: "Temido(a)", valor: 20 },
-      { option: "Invejado(a)", valor: 20 },
-      { option: "Elogiado(a)", valor: 20 },
-    ],
-  }),
-  (questao2 = {
-    questao: "Qual das matérias a seguir voçê gostaria de estudas?",
-    alternativas: [
-      { option: "Feitiços", valor: 10 },
-      { option: "Poções", valor: 10 },
-      { option: "História da magia", valor: 10 },
-      { option: "Astronomia", valor: 10 },
-      { option: "Herbologia", valor: 10 },
-    ],
-  }),
-  (questao3 = {
-    questao:
-      "Tarde da noite caminhando sozinho, voçê escuta um grito pedindo socorro, o que voçê faz?",
-    alternativas: [
-      { option: "Se esconde, para pensar em quais feitiços usar", valor: 10 },
-      {
-        option: "Vai de confronto ao pedido de socorro para ajudar",
-        valor: 10,
-      },
-      {
-        option:
-          "Continua seu caminho, preparado para qualquer inimigo a frente",
-        valor: 10,
-      },
-      {
-        option: "Decide ajudar, tomando cuidado para não ser percebido",
-        valor: 10,
-      },
-    ],
-  }),
-  (questao4 = {
-    questao: "Escolha um animal para passar as aulas com voçê",
-    alternativas: [
-      { option: "Gato", valor: 10 },
-      { option: "Coruja", valor: 10 },
-      { option: "Rato", valor: 10 },
-      { option: "Sapo", valor: 10 },
-      { option: "Coelho", valor: 10 },
-    ],
-  }),
-  (questao5 = {
-    questao: "Qual dos valores abaixo voçê prefere?",
-    alternativas: [
-      { option: "Coragem", valor: 30 },
-      { option: "Cordialidade", valor: 30 },
-      { option: "Sabedoria", valor: 30 },
-      { option: "Ambição", valor: 30 },
-    ],
-  }),
-];*/
-
+var grifinoria = [];
+var sonserina = [];
+var corvinal = [];
+var lufalufa = [];
 function next() {
   cont1.style.display = "none";
   cont2.style.display = "flex";
 }
 
 function next2(params) {
+  var qestao = document.getElementsByName("questao");
+  for (var i = 0; i < qestao.length; i++) {
+    if (qestao[i].checked) {
+      if (qestao[i] == document.getElementById("qt1_Gr")) {
+        grifinoria.push(Number(qestao[i].value));
+        console.log(grifinoria);
+      }
+      if (qestao[i] == document.getElementById("qt1_Co")) {
+        corvinal.push(Number(qestao[i].value));
+        console.log(corvinal);
+      }
+      if (qestao[i] == document.getElementById("qt1_So")) {
+        sonserina.push(Number(qestao[i].value));
+        console.log(sonserina);
+      }
+      if (qestao[i] == document.getElementById("qt1_Lu")) {
+        lufalufa.push(Number(qestao[i].value));
+        console.log(lufalufa);
+      }
+      if (qestao[i] == document.getElementById("qt1_todas")) {
+        lufalufa.push(Number(qestao[i].value));
+        sonserina.push(Number(qestao[i].value));
+        corvinal.push(Number(qestao[i].value));
+        grifinoria.push(Number(qestao[i].value));
+      }
+    }
+  }
+
   cont2.style.display = "none";
   cont3.style.display = "flex";
 }
 function next3(params) {
+  var qestao = document.getElementsByName("questao2");
+  for (var i = 0; i < qestao.length; i++) {
+    if (qestao[i].checked) {
+      if (qestao[i] == document.getElementById("qt2_Gr")) {
+        grifinoria.push(Number(qestao[i].value));
+        console.log(grifinoria);
+      }
+      if (qestao[i] == document.getElementById("qt2_Co")) {
+        corvinal.push(Number(qestao[i].value));
+        console.log(corvinal);
+      }
+      if (qestao[i] == document.getElementById("qt2_So")) {
+        sonserina.push(Number(qestao[i].value));
+        console.log(sonserina);
+      }
+      if (qestao[i] == document.getElementById("qt2_Lu")) {
+        lufalufa.push(Number(qestao[i].value));
+        console.log(lufalufa);
+      }
+      if (qestao[i] == document.getElementById("qt2_todas")) {
+        lufalufa.push(Number(qestao[i].value));
+        sonserina.push(Number(qestao[i].value));
+        corvinal.push(Number(qestao[i].value));
+        grifinoria.push(Number(qestao[i].value));
+      }
+    }
+  }
+
   cont3.style.display = "none";
   cont4.style.display = "flex";
 }
 function next4(params) {
+  var qestao = document.getElementsByName("questao3");
+  for (var i = 0; i < qestao.length; i++) {
+    if (qestao[i].checked) {
+      if (qestao[i] == document.getElementById("qt3_Gr")) {
+        grifinoria.push(Number(qestao[i].value));
+        console.log(grifinoria);
+      }
+      if (qestao[i] == document.getElementById("qt3_Co")) {
+        corvinal.push(Number(qestao[i].value));
+        console.log(corvinal);
+      }
+      if (qestao[i] == document.getElementById("qt3_So")) {
+        sonserina.push(Number(qestao[i].value));
+        console.log(sonserina);
+      }
+      if (qestao[i] == document.getElementById("qt3_Lu")) {
+        lufalufa.push(Number(qestao[i].value));
+        console.log(lufalufa);
+      }
+    }
+  }
+
   cont4.style.display = "none";
   cont5.style.display = "flex";
 }
 function next5(params) {
+  var qestao = document.getElementsByName("questao4");
+  for (var i = 0; i < qestao.length; i++) {
+    if (qestao[i].checked) {
+      if (qestao[i] == document.getElementById("qt4_Gr")) {
+        grifinoria.push(Number(qestao[i].value));
+        console.log(grifinoria);
+      }
+      if (qestao[i] == document.getElementById("qt4_Co")) {
+        corvinal.push(Number(qestao[i].value));
+        console.log(corvinal);
+      }
+      if (qestao[i] == document.getElementById("qt4_So")) {
+        sonserina.push(Number(qestao[i].value));
+        console.log(sonserina);
+      }
+      if (qestao[i] == document.getElementById("qt4_Lu")) {
+        lufalufa.push(Number(qestao[i].value));
+        console.log(lufalufa);
+      }
+      if (qestao[i] == document.getElementById("qt4_todas")) {
+        lufalufa.push(Number(qestao[i].value));
+        sonserina.push(Number(qestao[i].value));
+        corvinal.push(Number(qestao[i].value));
+        grifinoria.push(Number(qestao[i].value));
+      }
+    }
+  }
+
   cont5.style.display = "none";
   cont6.style.display = "flex";
 }
 function next6(params) {
-  sectionMap.style.display = "none";
+  var qestao = document.getElementsByName("questao5");
+  for (var i = 0; i < qestao.length; i++) {
+    if (qestao[i].checked) {
+      if (qestao[i] == document.getElementById("qt5_Gr")) {
+        grifinoria.push(Number(qestao[i].value));
+        console.log(grifinoria);
+      }
+      if (qestao[i] == document.getElementById("qt5_Co")) {
+        corvinal.push(Number(qestao[i].value));
+        console.log(corvinal);
+      }
+      if (qestao[i] == document.getElementById("qt5_So")) {
+        sonserina.push(Number(qestao[i].value));
+        console.log(sonserina);
+      }
+      if (qestao[i] == document.getElementById("qt5_Lu")) {
+        lufalufa.push(Number(qestao[i].value));
+        console.log(lufalufa);
+      }
+    }
+  }
+
+  cont6.style.display = "none";
   sectionHog.style.display = "block";
 }
